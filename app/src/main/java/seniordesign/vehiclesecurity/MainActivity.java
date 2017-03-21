bc85d0e5d4642d3b7b693779828eaa8bf920431a
Ben <benhochstadt@gmail.com>
2017-03-21 00:04:36 -0400

    4 Buttons - Interface with pi 192.168.1.20/<Test_Program_1.php - Test_Program_5.php>

--- app/src/main/java/seniordesign/vehiclesecurity/MainActivity.java
+++ app/src/main/java/seniordesign/vehiclesecurity/MainActivity.java
@@ -1,6 +1,7 @@
 package seniordesign.vehiclesecurity;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,14 +11,19 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends Activity{
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private String web_server_ip = "192.168.1.124";
+    private String web_server_protocol = "http://";
+    private String web_server_address = "192.168.1.20/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,29 +64,125 @@ public class MainActivity extends Activity{
     }
 
     public void onButton1(View view) throws IOException {
-        Log.d(TAG, "Button 1 clicked");
-        //URL url = new URL("192.168.1.124/Test_Program_1.php");
-        //HttpURLConnection client = (HttpURLConnection) url.openConnection();
-        //client.setRequestMethod("GET");
+        Log.d("MAIN", "Button 1 clicked");
+        Log.d("MAIN", web_server_protocol + web_server_address
+                + "Test_Program_1.php");
+        new AsyncNetworkHandler().execute(web_server_protocol + web_server_address +
+                "Test_Program_1.php");
     }
 
     public void onButton2(View view)
     {
-        Log.d(TAG, "Button 2 clicked");
+        Log.d("MAIN", "Button 2 clicked");
+        new AsyncNetworkHandler().execute(web_server_protocol + web_server_address
+                + "Test_Program_2.php");
     }
 
     public void onButton3(View view)
     {
-        Log.d(TAG, "Button 3 clicked");
+        Log.d("MAIN", "Button 3 clicked");
+        new AsyncNetworkHandler().execute(web_server_protocol + web_server_address
+                + "Test_Program_3.php");
     }
 
     public void onButton4(View view)
     {
-        Log.d(TAG, "Button 4 clicked");
+        Log.d("MAIN", "Button 4 clicked");
+        new AsyncNetworkHandler().execute(web_server_protocol + web_server_address
+                + "Test_Program_4.php");
+    }
+
+    public void onButton5(View view)
+    {
+        Log.d("MAIN", "Button 5 clicked");
+        new AsyncNetworkHandler().execute(web_server_protocol + web_server_address
+                + "Test_Program_5.php");
     }
 
     public void setWebServerIP(String url)
     {
-        web_server_ip = url;
+        web_server_address = url;
     }
 }
+
+class AsyncNetworkHandler extends AsyncTask<String, Integer, Double>
+{
+    @Override
+    protected Double doInBackground(String... params)
+    {
+        sendRequest(params[0]);
+        return null;
+    }
+
+
+    private void sendRequest(String address)
+    {
+        //Log.d("MAIN", "sendRequest beginning...");
+        try
+        {
+            //URL mycoo = new URL("http://192.168.1.20/Test_Program_1.php");
+            URL url = new URL(address);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.getInputStream();
+            conn.disconnect();
+
+
+
+            //if(conn.getResponseCode() == HttpURLConnection.HTTP_OK)
+            //    Log.d("MAIN", "we got our HTTP OK");
+
+            //conn.setRequestMethod("POST");
+
+            // Process for connection --
+            /*
+            1. Set Request Properties
+            2. (Optional) getOutputStream(), write to the stream, close the stream.
+            3. getInputStream(), read from the stream, close the stream.
+            http://stackoverflow.com/questions/4844535/why-do-you-have-to-call-urlconnectiongetinputstream-to-be-able-to-write-out-to
+            //*/
+
+            /*
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes("mydata=hello");
+            wr.flush();
+            wr.close();
+            //*/
+
+            /*
+            InputStream is = conn.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+
+            Log.d("MAIN", response.toString());
+            conn.disconnect();
+            //*/
+        }
+        catch(Exception e)
+        {
+            Log.d("MAIN", e.toString());
+        }
+
+
+    }
+
+    // On completion
+    protected void onPostExecute(Double result)
+    {
+
+    }
+
+    // On progress
+    protected void onProgressUpdate(Integer... progress)
+    {
+
+    }
+}
+
